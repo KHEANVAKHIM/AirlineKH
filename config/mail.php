1182efaeeb631b975e4eeed10cab591baddac40a
KHEANVAKHIM <vakhimkhean@gmail.com>
2026-09-17 13:26:27 +0700

    fix(mail): set strict 3s timeout on smtp to eliminate long loading on blocked cloud ports

--- config/mail.php
+++ config/mail.php
@@ -46,7 +46,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'timeout' => null,
+            'timeout' => (int) env('MAIL_TIMEOUT', 3),
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
